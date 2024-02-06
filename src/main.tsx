9e60e89ed0b7689256a701dbd7fa0757c1806b1a
Whoknow77 <gnsdh86166@gmail.com>
2024-02-06 20:53:41 +0900

    [Chore 🚀] 초기 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import GlobalStyles from "./styles/GlobalStyles.tsx"
+import { ThemeProvider } from "styled-components"
+import { theme } from "./styles/theme.ts"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 1000 * 60 * 3,
+      staleTime: 1000 * 60,
+    },
+    mutations: {
+      gcTime: 1000 * 60 * 3,
+    },
+  },
+})
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
