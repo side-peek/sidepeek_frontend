9eb291949db476b7d977ddb41d066893e6072e21
이종혁 <whdgur57117@gmail.com>
2024-02-10 23:38:49 +0900

    [Chore 🚀] Chakra UI 설정 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from 'react-router-dom'
 
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-import { ThemeProvider } from 'styled-components'
-
-import GlobalStyles from '@/styles/GlobalStyles'
-
 import { theme } from '@styles/theme'
+
+import GlobalStyles from './styles/GlobalStyles'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -24,10 +24,11 @@ const queryClient = new QueryClient({
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
+      <ChakraProvider theme={theme}>
         <GlobalStyles />
+        <ColorModeScript initialColorMode='light' />
         <Outlet />
-      </ThemeProvider>
+      </ChakraProvider>
     </QueryClientProvider>
   )
 }
