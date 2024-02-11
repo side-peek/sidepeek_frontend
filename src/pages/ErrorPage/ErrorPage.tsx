e382b26293d5d0b6968d859c8118f9afcc75b568
Whoknow77 <gnsdh86166@gmail.com>
2024-02-11 20:48:10 +0900

    [Feat ✏️] 에러페이지 이미지 추가

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,5 +1,18 @@
+import errorgif from "@assets/images/error.gif"
+
 const ErrorPage = () => {
-  return <div>Error Page</div>
+  return (
+    <img
+      src={errorgif}
+      alt="에러 페이지 움짤"
+      style={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translateX(-50%) translateY(-50%)",
+      }}
+    />
+  )
 }
 
 export default ErrorPage
