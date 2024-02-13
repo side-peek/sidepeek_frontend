009dc46573eb4e2e10a065d5700c1e4bf2a3fdba
genTe <128919388+wdgWon@users.noreply.github.com>
2024-02-14 01:18:07 +0900

    [Feat ✏️] 로그인 UI 구현

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,5 +1,148 @@
+import { Link, useNavigate } from "react-router-dom"
+
+import {
+  AspectRatio,
+  Box,
+  Button,
+  Center,
+  Divider,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Image,
+  Input,
+  Spacer,
+  Text,
+} from "@chakra-ui/react"
+
+import githubSVG from "@assets/github.svg"
+import backSpaceSVG from "@assets/keyboard_backspace.svg"
+import sidePeekSVG from "@assets/sidepeek_logo.svg"
+
 const LoginPage = () => {
-  return <div>LoginPage</div>
+  const navigate = useNavigate()
+  return (
+    <Center h="full">
+      <Box
+        position="relative"
+        w="30rem">
+        <Button
+          position="absolute"
+          top="0"
+          left="0"
+          w="3rem"
+          h="2rem"
+          p="0rem"
+          background="transparent"
+          onClick={() => navigate(-1)}>
+          <Image
+            src={backSpaceSVG}
+            alt="backspace"
+          />
+        </Button>
+        <Flex
+          direction="column"
+          gap="1.6rem"
+          justifyContent="center"
+          alignItems="stretch">
+          <Center>
+            <Link to="/">
+              <AspectRatio
+                w="15rem"
+                ratio={5 / 1.3}>
+                <Image
+                  src={sidePeekSVG}
+                  alt="side-peek logo"
+                />
+              </AspectRatio>
+            </Link>
+          </Center>
+          {/* 로그인 입력 양식 */}
+          {/* email */}
+          <FormControl>
+            <FormLabel>
+              <Flex align="center">
+                <Text
+                  px=".8rem"
+                  fontSize="1.2rem"
+                  fontWeight="bold">
+                  이메일
+                </Text>
+                <Spacer />
+                {/* <FormHelperText>이곳은 에러메세지 자리입니다.</FormHelperText> */}
+              </Flex>
+            </FormLabel>
+            <Input type="email" />
+          </FormControl>
+          {/* paasword */}
+          <FormControl>
+            <FormLabel>
+              <Flex align="center">
+                <Text
+                  px=".8rem"
+                  fontSize="1.2rem"
+                  fontWeight="bold">
+                  비밀번호
+                </Text>
+                <Spacer />
+                {/* <FormHelperText>이곳은 에러메세지 자리입니다.</FormHelperText> */}
+              </Flex>
+            </FormLabel>
+            <Input type="password" />
+          </FormControl>
+          <Button
+            p="2rem"
+            colorScheme="blue"
+            fontSize="1.8rem">
+            로그인
+          </Button>
+          <Divider />
+          <Box>
+            <Heading
+              as="h3"
+              fontSize="1.2rem"
+              fontWeight="bold">
+              소셜 계정으로 로그인
+            </Heading>
+          </Box>
+          <Button
+            p="2rem"
+            fontSize="2rem"
+            color="white"
+            leftIcon={
+              <Image
+                src={githubSVG}
+                alt="github icon"
+                w={26}
+                h={26}
+              />
+            }
+            backgroundColor="#000"
+            borderRadius="full">
+            깃허브로 로그인
+          </Button>
+          <Flex
+            pt="1.4rem"
+            justifyContent="flex-end">
+            <Box
+              fontSize="1.2rem"
+              color="blue">
+              회원이 아니신가요?
+              <Link to="/signup">
+                <Text
+                  px="0.4rem"
+                  display="inline"
+                  decoration="underline">
+                  회원가입
+                </Text>
+              </Link>
+            </Box>
+          </Flex>
+        </Flex>
+      </Box>
+    </Center>
+  )
 }
 
 export default LoginPage
