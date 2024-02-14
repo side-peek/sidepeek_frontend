e9b6e2897e21d592b7cb07a1aad14e7eeaeaac9d
hellosonic-r <hellommmh@gmail.com>
2024-02-14 22:22:32 +0900

    [Feat ✏️] 기본 Input 공용 컴포넌트 구현

--- src/components/Input/CommonInput.tsx
+++ src/components/Input/CommonInput.tsx
@@ -1,7 +1,47 @@
-import { Input } from "@chakra-ui/react"
+import { AiOutlineSearch } from "react-icons/ai"
 
-const CommonInput = () => {
-  return <Input placeholder="basic"></Input>
+import { Input, InputGroup, InputRightElement, Stack } from "@chakra-ui/react"
+
+import { CommonInputProps } from "./types/types"
+
+const CommonInput = ({
+  // isInvalid = false,
+  width = "20rem",
+  size = "md",
+  register,
+  required,
+  placeholder,
+  variant = "outline",
+  stackStyle,
+  type,
+  borderRadius = "0rem",
+  ...rest
+}: CommonInputProps) => {
+  return (
+    <Stack style={stackStyle}>
+      <InputGroup width={width}>
+        <Input
+          // paddingLeft="1rem"
+          // isInvalid={isInvalid}
+          size={size}
+          variant={variant}
+          placeholder={placeholder}
+          required={required}
+          type={type}
+          borderRadius={borderRadius}
+          _focus={{ boxShadow: "none", borderColor: "black" }}
+          {...register}
+          {...rest}></Input>
+        <InputRightElement
+          pointerEvents="none"
+          paddingTop="1rem"
+          fontSize="1.5rem"
+          color="red">
+          <AiOutlineSearch />
+        </InputRightElement>
+      </InputGroup>
+    </Stack>
+  )
 }
 
 export default CommonInput
