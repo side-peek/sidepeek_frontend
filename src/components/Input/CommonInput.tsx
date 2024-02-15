5fccfe673d1900f06a8fdeec104bf1432804117a
hellosonic-r <hellommmh@gmail.com>
2024-02-16 00:07:58 +0900

    [Refactor ⚙️] Chakra UI Props로 대체

--- src/components/Input/CommonInput.tsx
+++ src/components/Input/CommonInput.tsx
@@ -1,57 +1,44 @@
-import { Input, InputGroup, Stack } from "@chakra-ui/react"
+import { Input, InputGroup } from "@chakra-ui/react"
 
 import InsideElement from "./components/InsideElement"
 import { CommonInputProps } from "./types/types"
 
 /**
- *
- * @param {string} type - 필수;
+ * @param {string} inputWidth - Input 너비; 20rem(default)
  * @param {object} register - 필수; Input 필드를 관리함; 다양한 validation 처리 가능
+ * @param {string} type - text(default)
  * @param {boolean} required - true(default) | false
  * @param {string} placeholder - 플레이스홀더
  * @param {string} variant - Input 스타일; outline(default)
- * @param {string} width - Input 너비; 20rem(default)
  * @param {string} size - Input 크기; md(default)
  * @param {string} fontSize - 폰트 크기; 1rem(default)
  * @param {string} borderRadius - 0rem(default)
- * @param {string} stackStyle - Input Wrapper에 스타일 속성을 지정할 수 있음
  * @param {React.ReactNode} children - <CommonInput> {children} </CommonInput>으로 Input 우측 내부에 아이콘 등의 요소를 추가할 수 있음
- * @param {...object} rest - Input에 스타일 속성을 적용하는 등 커스터마이징 가능
+ * @param {...object} props - Input에 스타일 속성을 적용하는 등 커스터마이징 가능
  * @returns {React.Element}
  */
 
 const CommonInput = ({
-  type,
+  inputWidth = "20rem",
   register,
-  required = true,
-  placeholder,
-  variant = "outline",
-  width = "20rem",
-  size = "md",
-  fontSize = "1rem",
-  borderRadius = "0rem",
-  stackStyle,
   children,
-  ...rest
+  ...props
 }: CommonInputProps) => {
   return (
-    <Stack style={stackStyle}>
-      <InputGroup width={width}>
-        <Input
-          type={type}
-          {...register}
-          required={required}
-          placeholder={placeholder}
-          variant={variant}
-          width={width}
-          size={size}
-          fontSize={fontSize}
-          borderRadius={borderRadius}
-          _focus={{ boxShadow: "none" }}
-          {...rest}></Input>
-        <InsideElement>{children}</InsideElement>
-      </InputGroup>
-    </Stack>
+    <InputGroup width={inputWidth}>
+      <Input
+        {...register}
+        type="text"
+        required={true}
+        placeholder="입력해주세요"
+        variant="outline"
+        size="md"
+        fontSize="1rem"
+        borderRadius="0rem"
+        _focus={{ boxShadow: "none" }}
+        {...props}></Input>
+      <InsideElement>{children}</InsideElement>
+    </InputGroup>
   )
 }
 
--- src/components/Input/types/types.ts
+++ src/components/Input/types/types.ts
@@ -1,17 +1,9 @@
-import { CSSProperties, ReactNode } from "react"
+import { PropsWithChildren } from "react"
 import { UseFormRegisterReturn } from "react-hook-form"
 
-export interface CommonInputProps {
-  type: "text" | "email" | "password"
+import { InputProps } from "@chakra-ui/react"
+
+export interface CommonInputProps extends PropsWithChildren, InputProps {
   register: UseFormRegisterReturn
-  required?: boolean
-  placeholder?: string
-  variant?: "outline" | "filled" | "flushed" | "unstyled"
-  width?: string
-  size?: "xs" | "sm" | "md" | "lg"
-  fontSize?: string
-  borderRadius?: string
-  stackStyle?: CSSProperties
-  children?: ReactNode
-  [key: string]: unknown
+  inputWidth?: string
 }
