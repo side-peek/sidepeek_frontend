b318c488df8fd8d3396262d2a837c29b4ac59912
Whoknow77 <gnsdh86166@gmail.com>
2024-02-15 11:38:13 +0900

    [Feat ✏️] 테스트 로그인 페이지 생성

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,5 +1,45 @@
+import { Button, Center } from "@chakra-ui/react"
+
+import AUTH_USER_INITIAL_USER_DATA from "@/constants/user"
+import useAuthStore from "@/stores/useAuthStore"
+
 const LoginPage = () => {
-  return <div>LoginPage</div>
+  const { setLogin, setLogout } = useAuthStore()
+
+  const handleLogin = () => {
+    setLogin(AUTH_USER_INITIAL_USER_DATA, "123")
+  }
+
+  const handleLogout = () => {
+    setLogout()
+  }
+
+  return (
+    <Center gap={2}>
+      <Button
+        bg="blue.100"
+        color="white"
+        size="lg"
+        borderRadius="1rem"
+        _hover={{ opacity: 0.5 }}
+        height="5rem"
+        width="15rem"
+        onClick={handleLogin}>
+        로그인
+      </Button>
+      <Button
+        bg="blue.100"
+        color="white"
+        size="lg"
+        borderRadius="1rem"
+        _hover={{ opacity: 0.5 }}
+        height="5rem"
+        width="15rem"
+        onClick={handleLogout}>
+        로그아웃
+      </Button>
+    </Center>
+  )
 }
 
 export default LoginPage
