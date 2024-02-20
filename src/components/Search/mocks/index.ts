3d3a455c14a7d2df9f65df1077715f4e3d7fe8c3
이종혁 <whdgur57117@gmail.com>
2024-02-21 03:25:24 +0900

    [Feat ✏️] 전체 스킬 불러오는 msw rest요청 추가

--- src/components/Search/mocks/index.ts
+++ src/components/Search/mocks/index.ts
@@ -5,7 +5,10 @@ import { DUMMY_SKILLS } from "./mockData"
 export const searchHandlers = [
   rest.get("/api/v1/skills", (req, res, ctx) => {
     const keyword = req.url.searchParams.get("keyword")
-    if (keyword && keyword.length >= 21) {
+    if (!keyword) {
+      return res(ctx.status(200), ctx.json(DUMMY_SKILLS))
+    }
+    if (keyword.length >= 21) {
       return res(
         ctx.status(400),
         ctx.json({
