d56da177f604537e6ae09d18c26c65b81bbbd831
이종혁 <whdgur57117@gmail.com>
2024-02-20 22:26:07 +0900

    [Change 🚜 ] 사용되는 위치로 mocks 파일 이동

---
+++ src/components/Search/mocks/index.ts
@@ -0,0 +1,18 @@
+import { rest } from "msw"
+
+import { DUMMY_SKILLS } from "./mockData"
+
+export const searchHandlers = [
+  rest.get("/api/v1/skills", (req, res, ctx) => {
+    const keyword = req.url.searchParams.get("keyword")
+    if (keyword && keyword.length >= 21) {
+      return res(
+        ctx.status(400),
+        ctx.json({
+          message: "최대 20자의 키워드로 검색할 수 있습니다",
+        }),
+      )
+    }
+    return res(ctx.status(200), ctx.json(DUMMY_SKILLS))
+  }),
+]
