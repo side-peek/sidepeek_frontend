81507c856117c104765baf8243ea4ad68c4e2406
이종혁 <whdgur57117@gmail.com>
2024-02-22 11:02:12 +0900

    [Feat ✏️] 기술 스택 Dummy data 추가

--- src/components/Search/mocks/mockData.ts
+++ src/components/Search/mocks/mockData.ts
@@ -12,5 +12,21 @@ export const DUMMY_SKILLS: { skills: Skill[] } = {
       name: "spring",
       iconImageUrl: "https://www.iconimageurl.com",
     },
+    {
+      id: 10,
+      name: "spring",
+      iconImageUrl: "https://www.iconimageurl.com",
+    },
+    {
+      id: 6,
+      name: "react query",
+      iconImageUrl: "https://www.iconimageurl.com",
+    },
+    { id: 9, name: "github", iconImageUrl: "https://www.iconimageurl.com" },
+    {
+      id: 11,
+      name: "github action",
+      iconImageUrl: "https://www.iconimageurl.com",
+    },
   ],
 }
