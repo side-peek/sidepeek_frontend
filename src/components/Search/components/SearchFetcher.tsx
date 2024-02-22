4d2c054a486e5557ea277b542db59d058f8aad21
이종혁 <whdgur57117@gmail.com>
2024-02-22 11:23:53 +0900

    [Feat ✏️] 데이터 불러오기 담당하는 SearchFetcher 컴포넌트 생성

---
+++ src/components/Search/components/SearchFetcher.tsx
@@ -0,0 +1,21 @@
+import { ReactNode, useEffect } from "react"
+
+import { useSearchQuery } from "../hooks/queries/useSearchQuery"
+import { useSearchContext } from "../stores/useSearchContext"
+
+const SearchFetcher = ({ children }: { children?: ReactNode }) => {
+  const { inputValue, setSearchList } = useSearchContext()
+  const { data } = useSearchQuery(inputValue)
+
+  useEffect(() => {
+    if (data === undefined) {
+      return
+    }
+    setSearchList(data)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data])
+
+  return children
+}
+
+export default SearchFetcher
