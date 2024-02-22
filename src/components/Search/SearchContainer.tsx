5d31daeba30634dc6ea84755de3172cb060e5f37
이종혁 <whdgur57117@gmail.com>
2024-02-22 11:32:20 +0900

    [Feat ✏️] Context와 컴포넌트를 담고 있는 Container 컴포넌트 생성

---
+++ src/components/Search/SearchContainer.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react"
+
+import { List, ListItem } from "@chakra-ui/react"
+import { Skill } from "api-models"
+
+import SearchBar from "./components/SearchBar"
+import SearchFetcher from "./components/SearchFetcher"
+import SearchLayout from "./components/SearchLayout"
+import { useInput } from "./hooks/useInput"
+import { SearchContext } from "./stores/SearchContext/SearchContext"
+
+const SearchContainer = () => {
+  const [isFocused, setIsFocused] = useState(false)
+  const [inputValue, onInput] = useInput("")
+  const [searchList, setSearchList] = useState<Skill[]>([])
+
+  return (
+    <SearchContext.Provider
+      value={{
+        isFocused,
+        setIsFocused,
+        inputValue,
+        onInput,
+        searchList,
+        setSearchList,
+      }}>
+      <SearchLayout width="200px">
+        <SearchBar placeholder="찾고싶은 기술을 입력해주세요" />
+        <SearchFetcher>
+          {isFocused && (
+            <List>
+              {searchList?.map((skill) => {
+                return <ListItem key={skill.id}>{skill.name}</ListItem>
+              })}
+              <ListItem>{inputValue}</ListItem>
+            </List>
+          )}
+        </SearchFetcher>
+      </SearchLayout>
+    </SearchContext.Provider>
+  )
+}
+
+export default SearchContainer
