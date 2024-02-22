29baaf1b8c00b339bf6ae31ac01254989c05a0d2
이종혁 <whdgur57117@gmail.com>
2024-02-22 13:24:17 +0900

    [Feat ✏️] ref 적용 위해 forwardRef로 래핑

--- src/components/Search/components/SearchLayout.tsx
+++ src/components/Search/components/SearchLayout.tsx
@@ -1,9 +1,19 @@
+import { forwardRef } from "react"
+
 import { Box, BoxProps } from "@chakra-ui/react"
 
 interface SearchLayoutProps extends BoxProps {}
+//
+const SearchLayout = forwardRef<HTMLDivElement, SearchLayoutProps>(
+  ({ ...props }, ref) => {
+    return (
+      <Box
+        {...props}
+        ref={ref}></Box>
+    )
+  },
+)
 
-const SearchLayout = ({ ...props }: SearchLayoutProps) => {
-  return <Box {...props}></Box>
-}
+SearchLayout.displayName = "SearchLayout"
 
 export default SearchLayout
