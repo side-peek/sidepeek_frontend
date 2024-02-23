50fd8583ddff6b7b48f6cc7aebbfc60ad3a7701c
이종혁 <whdgur57117@gmail.com>
2024-02-23 21:03:58 +0900

    [Change 🚜 ] 주석 제거

--- src/components/Search/components/SearchLayout.tsx
+++ src/components/Search/components/SearchLayout.tsx
@@ -3,7 +3,7 @@ import { forwardRef } from "react"
 import { Box, BoxProps } from "@chakra-ui/react"
 
 interface SearchLayoutProps extends BoxProps {}
-//
+
 const SearchLayout = forwardRef<HTMLDivElement, SearchLayoutProps>(
   ({ ...props }, ref) => {
     return (
