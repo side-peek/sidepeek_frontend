66ef7b4b598d2d2965106af50af68bf58b6b6146
hellosonic-r <hellommmh@gmail.com>
2024-02-24 13:42:07 +0900

    [Feat ✏️] MSW 더미 데이터 추가

--- src/pages/ProfilePage/mocks/mockData.ts
+++ src/pages/ProfilePage/mocks/mockData.ts
@@ -1,10 +1,11 @@
 export const DUMMY_USER_INFO = {
   userInfo: {
     nickname: "sehee",
-    introduction: "hi~ H~ I~!",
-    profileImageUrl: "profile_image_url",
+    introduction:
+      "안녕하세요, msw 더미데이터입니다. 안녕하세요, msw 더미데이터입니다.안녕하세요, msw 더미데이터입니다.안녕하세요, msw 더미데이터입니다.안녕하세요, msw 더미데이터입니다.",
+    profileImageUrl: "https://picsum.photos/200",
     job: "백엔드 개발자",
-    career: "0년차",
+    career: "1년차",
     githubUrl: "https://www.naver.com",
     blogUrl: "https://www.naver.com",
     techStacks: [
@@ -19,7 +20,119 @@ export const DUMMY_USER_INFO = {
         category: "프론트",
       },
       {
-        id: 21,
+        id: 22,
+        skill: {
+          id: 1,
+          name: "React",
+          iconImageUrl:
+            "https://cdn.iconscout.com/icon/free/png-512/free-react-1-282599.png?f=webp&w=256",
+        },
+        category: "프론트",
+      },
+      {
+        id: 23,
+        skill: {
+          id: 1,
+          name: "React",
+          iconImageUrl:
+            "https://cdn.iconscout.com/icon/free/png-512/free-react-1-282599.png?f=webp&w=256",
+        },
+        category: "프론트",
+      },
+      {
+        id: 24,
+        skill: {
+          id: 1,
+          name: "React",
+          iconImageUrl:
+            "https://cdn.iconscout.com/icon/free/png-512/free-react-1-282599.png?f=webp&w=256",
+        },
+        category: "프론트",
+      },
+      {
+        id: 25,
+        skill: {
+          id: 1,
+          name: "React",
+          iconImageUrl:
+            "https://cdn.iconscout.com/icon/free/png-512/free-react-1-282599.png?f=webp&w=256",
+        },
+        category: "프론트",
+      },
+      {
+        id: 26,
+        skill: {
+          id: 1,
+          name: "React",
+          iconImageUrl:
+            "https://cdn.iconscout.com/icon/free/png-512/free-react-1-282599.png?f=webp&w=256",
+        },
+        category: "프론트",
+      },
+      {
+        id: 27,
+        skill: {
+          id: 1,
+          name: "React",
+          iconImageUrl:
+            "https://cdn.iconscout.com/icon/free/png-512/free-react-1-282599.png?f=webp&w=256",
+        },
+        category: "프론트",
+      },
+
+      {
+        id: 28,
+        skill: {
+          id: 3,
+          name: "Spring",
+          iconImageUrl:
+            "https://cdn.iconscout.com/icon/free/png-512/free-react-1-282599.png?f=webp&w=256",
+        },
+        category: "백",
+      },
+      {
+        id: 29,
+        skill: {
+          id: 3,
+          name: "Spring",
+          iconImageUrl:
+            "https://cdn.iconscout.com/icon/free/png-512/free-react-1-282599.png?f=webp&w=256",
+        },
+        category: "백",
+      },
+
+      {
+        id: 30,
+        skill: {
+          id: 3,
+          name: "SpringSpring",
+          iconImageUrl:
+            "https://cdn.iconscout.com/icon/free/png-512/free-react-1-282599.png?f=webp&w=256",
+        },
+        category: "백",
+      },
+      {
+        id: 31,
+        skill: {
+          id: 3,
+          name: "Spring",
+          iconImageUrl:
+            "https://cdn.iconscout.com/icon/free/png-512/free-react-1-282599.png?f=webp&w=256",
+        },
+        category: "백",
+      },
+      {
+        id: 32,
+        skill: {
+          id: 3,
+          name: "Spring",
+          iconImageUrl:
+            "https://cdn.iconscout.com/icon/free/png-512/free-react-1-282599.png?f=webp&w=256",
+        },
+        category: "백",
+      },
+      {
+        id: 33,
         skill: {
           id: 3,
           name: "Spring",
