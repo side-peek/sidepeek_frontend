90d8a1392d90aadca237bf9b94f09542ac0630b0
Whoknow77 <gnsdh86166@gmail.com>
2024-02-25 18:15:47 +0900

    [Design 🎨] 슬라이드 내비게이션 밖으로 뺌

--- src/pages/ProjectDetailPage/components/ProjectDetailSummaryContent/ProjectDetailSummaryRight.tsx
+++ src/pages/ProjectDetailPage/components/ProjectDetailSummaryContent/ProjectDetailSummaryRight.tsx
@@ -1,10 +1,15 @@
-import { Box, Image } from "@chakra-ui/react"
+import { useRef } from "react"
+import { MdArrowBackIosNew } from "react-icons/md"
+import { MdArrowForwardIos } from "react-icons/md"
+
+import { Box, Icon, IconButton, Image } from "@chakra-ui/react"
 import { ProjectOverViewUrl } from "api-models"
 import "swiper/css"
 import "swiper/css/navigation"
 import "swiper/css/pagination"
 import { Keyboard, Mousewheel, Navigation, Pagination } from "swiper/modules"
 import { SwiperSlide } from "swiper/react"
+import { Swiper as SwiperCore } from "swiper/types"
 
 import { StyledSwiper } from "./SwiperSlide.styles"
 
@@ -13,7 +18,6 @@ interface ProjectDetailSummaryRightProps {
 }
 
 const swiperParams = {
-  navigation: true,
   loop: true,
   pagination: {
     clickable: true,
@@ -25,11 +29,16 @@ const swiperParams = {
 const ProjectDetailSummaryRight = ({
   overviewImageUrl,
 }: ProjectDetailSummaryRightProps) => {
+  const swiperRef = useRef<SwiperCore>()
+
   return (
     <Box
       maxW="43rem"
-      maxH="33rem">
-      <StyledSwiper {...swiperParams}>
+      maxH="33rem"
+      position="relative">
+      <StyledSwiper
+        {...swiperParams}
+        onBeforeInit={(swiper) => (swiperRef.current = swiper)}>
         {overviewImageUrl.map((overviewImg) => (
           <SwiperSlide key={overviewImg.id}>
             <Image
@@ -40,6 +49,47 @@ const ProjectDetailSummaryRight = ({
           </SwiperSlide>
         ))}
       </StyledSwiper>
+      <IconButton
+        aria-label="notification popover"
+        position="absolute"
+        top="44%"
+        left="-6%"
+        bgColor="#d9d9d9"
+        height="5rem"
+        width="5rem"
+        borderRadius="50%"
+        zIndex="5"
+        onClick={() => swiperRef.current?.slidePrev()}
+        icon={
+          <Icon
+            as={MdArrowBackIosNew}
+            w="3rem"
+            h="3rem"
+            color="#7a7a7a"
+          />
+        }
+      />
+
+      <IconButton
+        aria-label="notification popover"
+        position="absolute"
+        top="44%"
+        right="-6%"
+        bgColor="#d9d9d9"
+        height="5rem"
+        width="5rem"
+        borderRadius="50%"
+        zIndex="5"
+        onClick={() => swiperRef.current?.slideNext()}
+        icon={
+          <Icon
+            as={MdArrowForwardIos}
+            w="3rem"
+            h="3rem"
+            color="#7a7a7a"
+          />
+        }
+      />
     </Box>
   )
 }
--- src/pages/ProjectDetailPage/components/ProjectDetailSummaryContent/SwiperSlide.styles.ts
+++ src/pages/ProjectDetailPage/components/ProjectDetailSummaryContent/SwiperSlide.styles.ts
@@ -22,19 +22,6 @@ export const StyledSwiper = styled(Swiper)`
     height: 100%;
     object-fit: cover;
   }
-
-  .swiper-button-prev,
-  .swiper-button-next {
-    background-color: #d9d9d9;
-    height: 5rem;
-    width: 5rem;
-    border-radius: 50%;
-    color: #7a7a7a !important;
-  }
-
-  .swiper-button-prev:after,
-  .swiper-button-next:after {
-    font-size: 2.5rem !important;
-    font-weight: 900 !important;
-  }
 `
+
+export const NextButton = styled.button``
