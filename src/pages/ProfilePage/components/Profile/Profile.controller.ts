084848dac0850d8351d4311122d2b21d28255371
hellosonic-r <hellommmh@gmail.com>
2024-02-26 11:18:55 +0900

    [Refactor ⚙️] Profile Controller 파일 분리

---
+++ src/pages/ProfilePage/components/Profile/Profile.controller.ts
@@ -0,0 +1,9 @@
+import { NavigateFunction } from "react-router-dom"
+
+export const handleNewProject = (navigate: NavigateFunction) => {
+  navigate("/test")
+}
+
+export const handleEditProfile = (navigate: NavigateFunction) => {
+  navigate("/test")
+}
