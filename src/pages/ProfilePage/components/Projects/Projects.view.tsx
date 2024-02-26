5d8e4bd922a5523fbe20e616154b087ee1354e51
hellosonic-r <hellommmh@gmail.com>
2024-02-26 13:00:35 +0900

    [Refactor ⚙️] StyledTab 분리

--- src/pages/ProfilePage/components/Projects/Projects.view.tsx
+++ src/pages/ProfilePage/components/Projects/Projects.view.tsx
@@ -1,6 +1,5 @@
 import {
   Box,
-  Tab,
   TabList,
   TabPanel,
   TabPanels,
@@ -8,34 +7,32 @@ import {
   useMediaQuery,
 } from "@chakra-ui/react"
 
+import StyledTab from "@pages/ProfilePage/styles/StyledTab"
+
 import ProjectsCommented from "./ProjectsCommented"
 import ProjectsLiked from "./ProjectsLiked"
 import ProjectsOwned from "./ProjectsOwned"
 
 const ProjectsView = () => {
-  const [isLargerThan1200] = useMediaQuery("(min-width: 1200px)")
-
+  const [isLargerThan500] = useMediaQuery("(min-width: 500px)")
   return (
     <Box
       height="100%"
       mt="2rem">
       <Tabs
         size="lg"
+        isFitted={!isLargerThan500}
         variant="enclosed">
         <TabList
           h="6rem"
           sx={{
             "& > *": {
-              fontSize: isLargerThan1200 ? "1.8rem" : "1rem",
-              px: "3rem",
-              borderTopLeftRadius: isLargerThan1200 ? "20px" : "20px",
-              borderTopRightRadius: isLargerThan1200 ? "20px" : "20px",
               _selected: { fontFamily: "SCDream_Bold", color: "#000000" },
             },
           }}>
-          <Tab>내 프로젝트</Tab>
-          <Tab>좋아요한 프로젝트</Tab>
-          <Tab>댓글단 프로젝트</Tab>
+          <StyledTab>내 프로젝트</StyledTab>
+          <StyledTab>좋아요한 프로젝트</StyledTab>
+          <StyledTab>댓글단 프로젝트</StyledTab>
         </TabList>
         <TabPanels>
           <TabPanel>
