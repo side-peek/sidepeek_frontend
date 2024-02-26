60a1743d1224b78df318f120370344b4676008bb
Whoknow77 <gnsdh86166@gmail.com>
2024-02-26 11:29:18 +0900

    [Design 🎨] 전체 레이아웃 수정

--- src/pages/ProjectDetailPage/components/ProjectDetailSummary.tsx
+++ src/pages/ProjectDetailPage/components/ProjectDetailSummary.tsx
@@ -1,4 +1,4 @@
-import { Center, Flex } from "@chakra-ui/react"
+import { Box, Center, Flex } from "@chakra-ui/react"
 import { Project } from "api-models"
 
 import ProjectDetailSummaryContent from "./ProjectDetailSummaryContent/ProjectDetailSummaryContent"
@@ -21,35 +21,36 @@ const ProjectDetailSummary = ({ projects }: ProjectDetailSummaryProps) => {
     overview,
   } = projects[0]
   return (
-    <Flex
-      maxW="128rem"
-      bg="#f5f5f5"
-      flexDirection="column"
-      m="0 auto"
-      p="2rem">
-      <Flex flexDirection="column">
-        <ProjectDetailSummaryTop
-          likeCount={likeCount}
-          viewCount={viewCount}
-          commentCount={commentCount}
-        />
+    <Box bg="#f5f5f5">
+      <Flex
+        maxW="128rem"
+        flexDirection="column"
+        m="0 auto"
+        p="2rem">
+        <Flex flexDirection="column">
+          <ProjectDetailSummaryTop
+            likeCount={likeCount}
+            viewCount={viewCount}
+            commentCount={commentCount}
+          />
 
-        <Center
-          flexDirection="column"
-          gap="5rem">
-          <ProjectDetailSummaryTitle
-            name={name}
-            subName={subName}
-          />
-          <ProjectDetailSummaryContent
-            overviewImageUrl={overviewImageUrl}
-            overview={overview}
-            deployUrl={deployUrl}
-            githubUrl={githubUrl}
-          />
-        </Center>
+          <Center
+            flexDirection="column"
+            gap="5rem">
+            <ProjectDetailSummaryTitle
+              name={name}
+              subName={subName}
+            />
+            <ProjectDetailSummaryContent
+              overviewImageUrl={overviewImageUrl}
+              overview={overview}
+              deployUrl={deployUrl}
+              githubUrl={githubUrl}
+            />
+          </Center>
+        </Flex>
       </Flex>
-    </Flex>
+    </Box>
   )
 }
 
