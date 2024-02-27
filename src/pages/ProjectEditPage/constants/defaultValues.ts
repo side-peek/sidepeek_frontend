780237c57e90c0fc7a9c6b2d2bfecf0978e10e54
이종혁 <whdgur57117@gmail.com>
2024-02-27 17:51:46 +0900

    [Feat ✏️] Project Form에 필요한 입력값들의 타입과 초기값 설정

---
+++ src/pages/ProjectEditPage/constants/defaultValues.ts
@@ -0,0 +1,12 @@
+import { ProjectFormValues } from "../types/ProjectFormValues"
+
+export const ProjectFormDefaultValues: ProjectFormValues = {
+  name: "",
+  subName: "",
+  overview: "",
+  thumbnailUrl: "",
+  githubUrl: "",
+  deployUrl: "",
+  startDate: "",
+  endDate: "",
+}
---
+++ src/pages/ProjectEditPage/types/ProjectFormValues.ts
@@ -0,0 +1,11 @@
+//TODO : 내용 추가해서 최종적으로는 Project 타입을 상속받는 방향으로 수정
+export type ProjectFormValues = {
+  name: string
+  subName: string
+  overview: string
+  thumbnailUrl: string
+  githubUrl: string
+  deployUrl: string
+  startDate: string //2002-02
+  endDate: string //동일
+}
