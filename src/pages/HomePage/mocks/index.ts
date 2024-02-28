b1c84f85ef50dfaaea067b3f49ae8cb8f113d788
Seung Min Lee <74231194+miloul@users.noreply.github.com>
2024-02-28 12:55:26 +0900

    [Rename 🏷️] allProjectHandlers로 이름 변경

--- src/pages/HomePage/mocks/index.ts
+++ src/pages/HomePage/mocks/index.ts
@@ -4,10 +4,10 @@ import { ENDPOINTS } from "@constants/endPoints"
 
 import { mockData } from "./mockData"
 
-const projectHandlers = [
+const allProjectHandlers = [
   rest.get(ENDPOINTS.GET_ALL_PROJECTS, (_, res, ctx) => {
     return res(ctx.status(200), ctx.json(mockData))
   }),
 ]
 
-export default projectHandlers
+export default allProjectHandlers
