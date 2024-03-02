9b52524f85ae8eb5b6a91090274d84601f68e733
hellosonic-r <hellommmh@gmail.com>
2024-03-02 12:14:35 +0900

    [Feat ✏️] ProfileEditPage 내 버튼 UI 구현

--- src/pages/ProfileEditPage/ProfileEditPage.tsx
+++ src/pages/ProfileEditPage/ProfileEditPage.tsx
@@ -1,20 +1,42 @@
-import { StackDivider, VStack } from "@chakra-ui/react"
+import { Button, HStack, StackDivider, VStack } from "@chakra-ui/react"
 
 import ProfileCard from "./components/ProfileCard"
 import ProfileIntroduction from "./components/ProfileIntroduction"
 
 const ProfileEditPage = () => {
   return (
-    <VStack
-      w="80%"
-      h="100vh"
-      bg="yellow"
-      align="start"
-      m="auto"
-      divider={<StackDivider borderColor="blue" />}>
-      <ProfileCard />
-      <ProfileIntroduction />
-    </VStack>
+    <>
+      <VStack
+        w="80%"
+        h="100vh"
+        bg="yellow"
+        align="start"
+        m="auto"
+        divider={<StackDivider borderColor="blue" />}>
+        <ProfileCard />
+        <ProfileIntroduction />
+        <HStack>
+          <Button
+            w="11rem"
+            h="3.5rem"
+            fontSize="1.3rem"
+            color="white"
+            bg="blue.100"
+            borderRadius="10px">
+            비밀번호 변경
+          </Button>
+          <Button
+            w="11rem"
+            h="3.5rem"
+            fontSize="1.3rem"
+            color="white"
+            bg="blue.100"
+            borderRadius="10px">
+            변경내용 저장
+          </Button>
+        </HStack>
+      </VStack>
+    </>
   )
 }
 
