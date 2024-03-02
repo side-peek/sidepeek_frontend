e9f367352c857ce84870c0951b564721d10e770c
hellosonic-r <hellommmh@gmail.com>
2024-03-02 11:55:21 +0900

    [Feat ✏️] Stack 구분선 추가

--- src/pages/ProfileEditPage/ProfileEditPage.tsx
+++ src/pages/ProfileEditPage/ProfileEditPage.tsx
@@ -1,14 +1,19 @@
-import { Flex } from "@chakra-ui/react"
+import { StackDivider, VStack } from "@chakra-ui/react"
+
+import ProfileCard from "./components/ProfileCard"
 
 const ProfileEditPage = () => {
   return (
-    <Flex
+    <VStack
       w="80%"
       h="100vh"
       bg="yellow"
-      margin="auto">
-      ProfileEditPage
-    </Flex>
+      align="start"
+      m="auto"
+      divider={<StackDivider borderColor="blue" />}>
+      <ProfileCard />
+      <ProfileCard />
+    </VStack>
   )
 }
 
