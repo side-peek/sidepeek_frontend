478b1bcc83d853245929ab090b4e62bcc67fba0d
hellosonic-r <hellommmh@gmail.com>
2024-03-02 16:31:48 +0900

    [Feat ✏️] 비밀번호 변경 모달 구현

--- src/pages/ProfileEditPage/ProfileEditPage.tsx
+++ src/pages/ProfileEditPage/ProfileEditPage.tsx
@@ -1,9 +1,17 @@
-import { Button, HStack, StackDivider, VStack } from "@chakra-ui/react"
+import {
+  Button,
+  HStack,
+  StackDivider,
+  VStack,
+  useDisclosure,
+} from "@chakra-ui/react"
 
+import ChangePWModal from "./components/ChangePWModal"
 import ProfileCard from "./components/ProfileCard"
 import ProfileIntroduction from "./components/ProfileIntroduction"
 
 const ProfileEditPage = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
   return (
     <>
       <VStack
@@ -17,6 +25,7 @@ const ProfileEditPage = () => {
         <ProfileIntroduction />
         <HStack>
           <Button
+            onClick={onOpen}
             w="11rem"
             h="3.5rem"
             fontSize="1.3rem"
@@ -34,6 +43,10 @@ const ProfileEditPage = () => {
             borderRadius="10px">
             변경내용 저장
           </Button>
+          <ChangePWModal
+            isOpen={isOpen}
+            onClose={onClose}
+          />
         </HStack>
       </VStack>
     </>
---
+++ src/pages/ProfileEditPage/components/ChangePWModal.tsx
@@ -0,0 +1,171 @@
+import { SubmitHandler, useForm } from "react-hook-form"
+
+import {
+  Box,
+  Button,
+  Flex,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Spacer,
+  Text,
+} from "@chakra-ui/react"
+
+import CommonInput from "@components/Input/CommonInput"
+
+interface FormValues {
+  currentPassword: string
+  newPassword: string
+  checkPassword: string
+}
+interface ModalProps {
+  isOpen: boolean
+  onClose: () => void
+}
+const ChangePWModal = ({ isOpen, onClose }: ModalProps) => {
+  const {
+    register,
+    setError,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({
+    mode: "onSubmit",
+  })
+  const onValid = () => {
+    // TODO: 비밀번호 변경 api 요청
+    console.log("1")
+    onClose()
+  }
+  const onInvalid = () => {
+    console.log("Submit 실패")
+  }
+
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    // TODO: 기존 비밀번호 api 불러와서 비교 후 다르면 setError하도록 해야함
+    const { newPassword, checkPassword } = data
+    if (newPassword !== checkPassword) {
+      setError("checkPassword", {
+        type: "password-mismatch",
+        message: "비밀번호가 일치하지 않습니다",
+      })
+      onInvalid()
+    } else {
+      onValid()
+    }
+  }
+
+  return (
+    <Modal
+      size="lg"
+      isOpen={isOpen}
+      onClose={onClose}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>비밀번호 변경</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {/* TODO: 기존 비밀번호 api 불러와서 비교 validation */}
+            <Flex
+              direction="column"
+              gap="1.5rem">
+              <Box>
+                <Flex alignItems="center">
+                  <Text fontSize="1.3rem">현재 비밀번호</Text>
+                  <Spacer />
+                  {errors.currentPassword?.message && (
+                    <Text color="red.100">
+                      {errors.currentPassword.message.toString()}
+                    </Text>
+                  )}
+                </Flex>
+
+                <CommonInput
+                  type="password"
+                  size="lg"
+                  register={register("currentPassword", {
+                    required: "현재 비밀번호를 입력해주세요",
+                    minLength: {
+                      value: 8,
+                      message: "현재 비밀번호는 8자 이상입니다",
+                    },
+                  })}
+                  placeholder="현재 비밀번호"
+                />
+              </Box>
+
+              <Box>
+                <Flex alignItems="center">
+                  <Text fontSize="1.3rem">새로운 비밀번호</Text>
+                  <Spacer />
+                  {errors.newPassword?.message && (
+                    <Text color="red.100">
+                      {errors.newPassword?.message.toString()}
+                    </Text>
+                  )}
+                </Flex>
+
+                <CommonInput
+                  type="password"
+                  size="lg"
+                  register={register("newPassword", {
+                    required: "새로운 비밀번호를 입력해주세요",
+                    minLength: {
+                      value: 8,
+                      message: "새로운 비밀번호는 8자 이상이어야 합니다",
+                    },
+                  })}
+                  placeholder="새로운 비밀번호"
+                />
+              </Box>
+
+              <Box>
+                <Flex alignItems="center">
+                  <Text fontSize="1.3rem">비밀번호 확인</Text>
+                  <Spacer />
+                  {errors.checkPassword?.message ? (
+                    <Text color="red.100">
+                      {errors.checkPassword?.message.toString()}
+                    </Text>
+                  ) : (
+                    <Text></Text>
+                  )}
+                </Flex>
+
+                <CommonInput
+                  type="password"
+                  size="lg"
+                  register={register("checkPassword", {
+                    required: "새로운 비밀번호를 한번 더 입력해주세요",
+                    minLength: {
+                      value: 8,
+                      message: "새로운 비밀번호는 8자 이상이어야 합니다",
+                    },
+                  })}
+                  placeholder="비밀번호 확인"
+                />
+              </Box>
+            </Flex>
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              type="submit"
+              w="6rem"
+              h="3.5rem"
+              fontSize="1.3rem"
+              color="white"
+              bg="blue.100"
+              borderRadius="10px">
+              변경
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </form>
+    </Modal>
+  )
+}
+export default ChangePWModal
