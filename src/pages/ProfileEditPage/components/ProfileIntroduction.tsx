59e0669b5ac12e7c017ea1a7b860968ed7c33212
hellosonic-r <hellommmh@gmail.com>
2024-03-02 19:12:40 +0900

    [Design 🎨] ProfileIntroduction 스타일 수정

--- src/pages/ProfileEditPage/components/ProfileIntroduction.tsx
+++ src/pages/ProfileEditPage/components/ProfileIntroduction.tsx
@@ -1,28 +1,49 @@
 import { FaSquarePen } from "react-icons/fa6"
 import { ImGithub } from "react-icons/im"
 
-import { Box, HStack, Text, Textarea } from "@chakra-ui/react"
+import { Box, HStack, Input, Text, Textarea } from "@chakra-ui/react"
 
 const ProfileIntroduction = () => {
   return (
-    <Box w="100%">
+    <Box
+      w="100%"
+      pb="2rem">
       <Text
         fontSize="xl"
-        fontFamily="SCDream_Bold">
+        fontFamily="SCDream_Bold"
+        margin="1.5rem 0 1.5rem 1rem">
         소개
       </Text>
       <Textarea
-        w="100%"
-        placeholder="Here is a sample placeholder"
+        height="10rem"
+        resize="none"
+        placeholder="소개글을 입력해주세요"
       />
-      <HStack>
-        <ImGithub size="2.6rem" />
-        <Textarea placeholder="GitHub 링크를 입력해주세요" />
-      </HStack>
-      <HStack>
-        <FaSquarePen size="2.7rem" />
-        <Textarea placeholder="Blog 링크를 입력해주세요" />
-      </HStack>
+
+      <Box mt="2rem">
+        <HStack mb="1rem">
+          <ImGithub size="2.6rem" />
+          <Input
+            variant="flushed"
+            height="3rem"
+            placeholder="GitHub 링크를 입력해주세요"
+            ml="0.5rem"
+            // borderRadius="0.8rem"
+            w="30rem"
+          />
+        </HStack>
+        <HStack>
+          <FaSquarePen size="2.65rem" />
+          <Input
+            variant="flushed"
+            height="3rem"
+            placeholder="Blog 링크를 입력해주세요"
+            ml="0.5rem"
+            // borderRadius="0.8rem"
+            w="30rem"
+          />
+        </HStack>
+      </Box>
     </Box>
   )
 }
