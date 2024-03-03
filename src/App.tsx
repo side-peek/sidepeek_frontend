517f6fba68d6f8aae625e338b9435aac89ca1ac7
genTe <128919388+wdgWon@users.noreply.github.com>
2024-03-04 02:13:51 +0900

    [Feat ✏️] 리액트쿼리 throwOnError 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from "react-router-dom"
 
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react"
+import { isAxiosError } from "axios"
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
@@ -13,9 +14,14 @@ const queryClient = new QueryClient({
       gcTime: 1000 * 60 * 3,
       staleTime: 1000 * 60,
       refetchOnWindowFocus: false,
+      throwOnError: (error) =>
+        isAxiosError(error) && error.response?.status === 401,
+      retry: 0,
     },
     mutations: {
       gcTime: 1000 * 60 * 3,
+      throwOnError: (error) =>
+        isAxiosError(error) && error.response?.status === 401,
     },
   },
 })
