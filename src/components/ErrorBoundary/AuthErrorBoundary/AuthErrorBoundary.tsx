20c8a2aa0c41c13680496e52eed022bd3e549a88
genTe <128919388+wdgWon@users.noreply.github.com>
2024-03-04 16:41:28 +0900

    [Refactoring ⚙️] 사용하지 않는 코드 제거 및 props로 reset만 전달

--- src/components/ErrorBoundary/AuthErrorBoundary/AuthErrorBoundary.tsx
+++ src/components/ErrorBoundary/AuthErrorBoundary/AuthErrorBoundary.tsx
@@ -1,6 +1,6 @@
-import { Component, ErrorInfo, PropsWithChildren } from "react"
+import { Component, PropsWithChildren } from "react"
 
-import { QueryErrorResetBoundaryValue } from "node_modules/@tanstack/react-query/build/modern/QueryErrorResetBoundary"
+import { isAxiosError } from "axios"
 
 import ConfirmModal from "./components/ConfirmModal"
 
@@ -8,11 +8,11 @@ type State = {
   hasError: boolean
   isOpen: boolean
   error: Error | null
-} & QueryErrorResetBoundaryValue
+}
 
-interface AuthErrorBoundaryProps
-  extends QueryErrorResetBoundaryValue,
-    PropsWithChildren {}
+interface AuthErrorBoundaryProps extends PropsWithChildren {
+  reset: () => void
+}
 
 class AuthErrorBoundary extends Component<AuthErrorBoundaryProps, State> {
   constructor(props: AuthErrorBoundaryProps) {
@@ -21,36 +21,29 @@ class AuthErrorBoundary extends Component<AuthErrorBoundaryProps, State> {
       hasError: false,
       isOpen: false,
       error: null,
-      ...props,
     }
   }
 
   static getDerivedStateFromError(error: Error) {
-    // 에러를 잡아서 state를 업데이트하여 다음 렌더링에서 폴백 UI를 보여줄 수 있게 합니다.
+    // 에러를 잡아서 state를 업데이트하여 다음 렌더링에서 모달을 보여줄 수 있게 합니다.
     return { hasError: true, error: error }
   }
 
-  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-    // 여기에 에러 리포팅 서비스에 에러를 기록하는 코드를 넣을 수 있습니다.
-    console.log("error bounary caught error", error, errorInfo)
-  }
-
   handleClose = () => {
-    this.state.reset()
+    this.props.reset()
     this.setState({ hasError: false, isOpen: false })
   }
 
   render() {
-    // 401 에러가 발생한 경우 사용자 정의 폴백 UI를 렌더링할 수 있습니다.
+    // 401 에러가 발생한 경우 children과 모달을 같이 렌더링합니다.
     return (
       <>
         {this.props.children}
-        {/* <Outlet /> */}
-        {this.state.hasError && (
-          // isAxiosError(this.state.error) &&
-          // this.state.error.response?.status === 401
-          <ConfirmModal onClose={this.handleClose} />
-        )}
+        {this.state.hasError &&
+          isAxiosError(this.state.error) &&
+          this.state.error.response?.status === 401 && (
+            <ConfirmModal onClose={this.handleClose} />
+          )}
       </>
     )
   }
