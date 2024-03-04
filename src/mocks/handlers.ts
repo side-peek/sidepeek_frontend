18a6a330265163df5d29cbb151822c3c3a3a091d
이종혁 <whdgur57117@gmail.com>
2024-03-04 15:14:43 +0900

    [Remove 🗑️] handler.ts 예시코드 삭제

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,5 +1,3 @@
-import { rest } from "msw"
-
 import { searchHandlers } from "@components/Search/mocks"
 
 import allProjectHandlers from "@pages/HomePage/mocks"
@@ -9,25 +7,6 @@ import { postEmailLogin } from "./auth/postEmailLogin.mock"
 import { postEmailRefresh } from "./auth/postEmailRefresh.mock"
 
 export const handlers = [
-  rest.get("/api/v1/skills", (_, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json({
-        skills: [
-          {
-            id: 2,
-            name: "spring",
-            iconImageUrl: "https://www.iconimageurl.com",
-          },
-          {
-            id: 3,
-            name: "swift",
-            iconImageUrl: "https://www.iconimageurl.com",
-          },
-        ],
-      }),
-    )
-  }),
   ...projectDetailHandlers,
   ...searchHandlers,
   ...allProjectHandlers,
