4de700d497fa431cf3bc0626bd09152a6b6639da
Whoknow77 <gnsdh86166@gmail.com>
2024-03-09 14:21:26 +0900

    [Refactoring ⚙️] 댓글 제목 추가

--- src/pages/ProjectDetailPage/components/Comments/Layout/CommentsLayout.tsx
+++ src/pages/ProjectDetailPage/components/Comments/Layout/CommentsLayout.tsx
@@ -1,22 +1,28 @@
 import { ReactNode } from "react"
 
-import { Stack } from "@chakra-ui/react"
+import { Stack, Text } from "@chakra-ui/react"
 
 interface CommentsLayoutProps {
   children: ReactNode
 }
 const CommentsLayout = ({ children }: CommentsLayoutProps) => {
   return (
-    <Stack
-      margin="0 auto"
-      w="100%"
-      maxW="128rem"
-      borderRadius="1rem"
-      border="1px"
-      borderColor="grey.400"
-      direction="column"
-      mt="10rem">
-      {children}
+    <Stack gap="2rem">
+      <Text
+        fontSize="3xl"
+        fontFamily="SCDream_Bold">
+        댓글
+      </Text>
+      <Stack
+        margin="0 auto"
+        w="100%"
+        maxW="128rem"
+        borderRadius="1rem"
+        border="1px"
+        borderColor="grey.400"
+        direction="column">
+        {children}
+      </Stack>
     </Stack>
   )
 }
