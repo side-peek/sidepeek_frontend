3c6ade4f02c32588463756edd076bfb230b123cd
이종혁 <whdgur57117@gmail.com>
2024-03-11 00:35:18 +0900

    [Feat ✏️] AvatarCard 컴포넌트에 사용되는 CardImage 컴포넌트 생성

---
+++ src/components/AvatarCard/Components/AvatarCardImage.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react"
+
+import { Avatar, AvatarProps } from "@chakra-ui/react"
+
+interface AvatarCardImageProps extends AvatarProps {
+  children?: ReactNode
+}
+
+const AvatarCardImage = ({ children, ...props }: AvatarCardImageProps) => {
+  return (
+    <Avatar
+      size="xl"
+      {...props}>
+      {children}
+    </Avatar>
+  )
+}
+
+export default AvatarCardImage
