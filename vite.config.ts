934c2d5b29e57110fc744fe2bea0342faa26393c
genTe <128919388+wdgWon@users.noreply.github.com>
2024-03-11 16:31:55 +0900

    [Feat ✏️] 서버에도 api 요청 보낼 수 있게 proxy 기능 추가

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,15 @@ import tsconfigPaths from "vite-tsconfig-paths"
 export default defineConfig(({ command }) => ({
   plugins: [react(), tsconfigPaths(), splitVendorChunkPlugin()],
   publicDir: command === "serve" ? "public" : false,
+  server: {
+    proxy: {
+      "/serve": {
+        target: "http://3.39.156.144:8080",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/serve/, ""),
+      },
+    },
+  },
   build: {
     chunkSizeWarningLimit: 1600,
     rollupOptions: {
