1a9b758224af2ad70699ec9b0c1188c79ac39433
hellosonic-r <hellommmh@gmail.com>
2024-03-12 11:02:54 +0900

    [Refactor ⚙️] getUserDetail api config 삭제

--- src/api/user/getUserDetail.ts
+++ src/api/user/getUserDetail.ts
@@ -1,19 +1,12 @@
 import { getUserDetailPayload, getUserDetailResponseType } from "api-models"
-import { AxiosRequestConfig } from "axios"
 
 import { ENDPOINTS } from "@constants/endPoints"
 
 import { baseInstance } from "../axiosInstance"
 
-export const getUserDetail = async (
-  { userId }: getUserDetailPayload,
-  config: AxiosRequestConfig = {},
-) => {
+export const getUserDetail = async ({ userId }: getUserDetailPayload) => {
   const { data } = await baseInstance.get<getUserDetailResponseType>(
     ENDPOINTS.GET_USER_PROFILE(userId),
-    {
-      ...config,
-    },
   )
 
   return data
