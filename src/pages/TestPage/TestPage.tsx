95ac60061866b603eea14d8b3867e1b893050b05
genTe <128919388+wdgWon@users.noreply.github.com>
2024-03-12 22:46:08 +0900

    [Change 🚜 ] TestPage 초기화

--- src/pages/TestPage/TestPage.tsx
+++ src/pages/TestPage/TestPage.tsx
@@ -1,21 +1,5 @@
-import { Button } from "@chakra-ui/button"
-import { Center } from "@chakra-ui/layout"
-
-import { useMutation } from "@tanstack/react-query"
-
-import { postEmailRefresh } from "@/api/auth/postEmailRefresh"
-
 const TestPage = () => {
-  // const axiosError = new AxiosError<string, string>("에러", "CUSTOM_ERROR")
-  const { mutate } = useMutation({
-    mutationFn: () => postEmailRefresh({ refreshToken: "" }),
-  })
-
-  return (
-    <Center>
-      <Button onClick={() => mutate()}>에러!</Button>
-    </Center>
-  )
+  return <>TestPage</>
 }
 
 export default TestPage
