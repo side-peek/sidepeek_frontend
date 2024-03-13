5acccbee9df55610fa4e66f3e0631ebcdc0b4b23
genTe <128919388+wdgWon@users.noreply.github.com>
2024-03-13 19:46:05 +0900

    [Feat ✏️] 옵션과 메세지들 상수로 분리

---
+++ src/pages/SignUpPage/constants/toastMessages.ts
@@ -0,0 +1,17 @@
+export const DOUBLE_CHECK_MESSAGE = {
+  EMAIL: {
+    ERROR: "이미 존재하는 이메일입니다.",
+    SUCCESS: "사용 가능한 이메일입니다.",
+  },
+  NICKNAME: {
+    ERROR: "이미 존재하는 닉네임입니다.",
+    SUCCESS: "사용 가능한 닉네임입니다.",
+  },
+}
+
+export const SUBMIT_MESSAGE = {
+  ERROR: {
+    EMAIL_CHECK: "이메일 중복 확인이 필요합니다.",
+    NICKNAME_CHECK: "닉네임 중복 확인이 필요합니다.",
+  },
+}
---
+++ src/pages/SignUpPage/constants/toastOptions.ts
@@ -0,0 +1,10 @@
+import { UseToastOptions } from "@chakra-ui/toast"
+
+export const errorToastOptions: UseToastOptions = {
+  position: "top-right",
+  containerStyle: {
+    maxWidth: "100%",
+    fontSize: "1.2rem",
+    fontFamily: "SCDream_Regular",
+  },
+}
