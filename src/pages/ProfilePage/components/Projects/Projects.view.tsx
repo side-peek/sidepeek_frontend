e6a357996afeb0d402463cfce5e96a44861988da
hellosonic-r <hellommmh@gmail.com>
2024-03-13 23:48:03 +0900

    [Refactor ⚙️] 내 프로젝트 Pagination api 연동

--- src/pages/ProfilePage/components/Projects/Projects.view.tsx
+++ src/pages/ProfilePage/components/Projects/Projects.view.tsx
@@ -1,5 +1,9 @@
+import { useState } from "react"
+
 import {
   Box,
+  Grid,
+  GridItem,
   TabList,
   TabPanel,
   TabPanels,
@@ -7,14 +11,38 @@ import {
   useMediaQuery,
 } from "@chakra-ui/react"
 
+import Pagination from "@components/Pagination/Pagination"
+import ProjectCard from "@components/ProjectCard/ProjectCard"
+
 import StyledTab from "@pages/ProfilePage/styles/StyledTab"
 
-import ProjectsCommented from "./ProjectsCommented"
-import ProjectsLiked from "./ProjectsLiked"
-import ProjectsOwned from "./ProjectsOwned"
+import { useUserProjects } from "./Projects.model"
+
+interface ProjectProperties {
+  id: number
+  name: string
+  subName: string
+  isLiked: boolean
+  thumbnailUrl: string
+  viewCount: number
+  likeCount: number
+}
 
 const ProjectsView = () => {
   const [isLargerThan500] = useMediaQuery("(min-width: 500px)")
+  const [page, setPage] = useState(0)
+  console.log(page)
+
+  const { data } = useUserProjects({
+    userId: 1,
+    type: "JOINED",
+    page: page - 1,
+    size: 12,
+  })
+
+  const totalProjectsCount = data?.totalElements
+  const projects = data?.data
+
   return (
     <Box
       height="100%"
@@ -31,19 +59,51 @@ const ProjectsView = () => {
             },
           }}>
           <StyledTab>내 프로젝트</StyledTab>
-          <StyledTab>좋아요한 프로젝트</StyledTab>
-          <StyledTab>댓글단 프로젝트</StyledTab>
+          {/* <StyledTab>좋아요한 프로젝트</StyledTab>
+          <StyledTab>댓글단 프로젝트</StyledTab> */}
         </TabList>
         <TabPanels>
           <TabPanel>
-            <ProjectsOwned />
+            {/* <ProjectsOwned /> */}
+            <Grid
+              mt="0.5rem"
+              templateColumns="repeat(auto-fill, minmax(24rem, 1fr))"
+              gap={0}>
+              {projects &&
+                projects.map(
+                  ({
+                    id,
+                    name,
+                    subName,
+                    isLiked,
+                    thumbnailUrl,
+                    viewCount,
+                    likeCount,
+                  }: ProjectProperties) => (
+                    <GridItem key={id}>
+                      <ProjectCard
+                        imgUrl={thumbnailUrl}
+                        viewCount={viewCount}
+                        heartCount={likeCount}
+                        isFullHeart={isLiked}
+                        title={name}
+                        content={subName}
+                      />
+                    </GridItem>
+                  ),
+                )}
+            </Grid>
           </TabPanel>
-          <TabPanel>
+          {/* <TabPanel>
             <ProjectsLiked />
           </TabPanel>
           <TabPanel>
             <ProjectsCommented />
-          </TabPanel>
+          </TabPanel> */}
+          <Pagination
+            totalProjectsCount={totalProjectsCount}
+            setPage={setPage}
+          />
         </TabPanels>
       </Tabs>
     </Box>
