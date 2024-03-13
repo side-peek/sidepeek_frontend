c8cb730f1f6549dfa251864b631efe54eae403dd
이종혁 <whdgur57117@gmail.com>
2024-03-14 03:01:39 +0900

    [Refactoring ⚙️] 기존 타입을 PostFormDataType으로 변경

--- src/api/projectFile/postProjectFiles.ts
+++ src/api/projectFile/postProjectFiles.ts
@@ -1,10 +1,12 @@
 import { postProjectFilesResponseType } from "api-models"
 
+import { PostFormDataType } from "@pages/ProjectEditPage/types/PostFormDataType"
+
 import { ENDPOINTS } from "@constants/endPoints"
 
 import { baseInstance } from "../axiosInstance"
 
-export const postProjectFiles = async (file: FormData) => {
+export const postProjectFiles = async (file: PostFormDataType) => {
   const { data } = await baseInstance.post<postProjectFilesResponseType>(
     ENDPOINTS.UPLOAD_PROJECT_FILES,
     file,
--- src/pages/ProjectEditPage/hooks/usePostFileMutation.ts
+++ src/pages/ProjectEditPage/hooks/usePostFileMutation.ts
@@ -4,12 +4,19 @@ import { AxiosError } from "axios"
 
 import { UseMutationOptions, useMutation } from "@tanstack/react-query"
 
+import { PostFormDataType } from "../types/PostFormDataType"
+import { TypedFormData } from "../types/TypedFormDataValue"
+
 export const usePostFileMutation = ({
   ...options
-}: UseMutationOptions<postProjectFilesResponseType, AxiosError, FormData>) => {
+}: UseMutationOptions<
+  postProjectFilesResponseType,
+  AxiosError,
+  PostFormDataType
+>) => {
   return useMutation({
     mutationKey: ["file"],
-    mutationFn: (file: FormData) => postProjectFiles(file),
+    mutationFn: (file: TypedFormData<{ file: File }>) => postProjectFiles(file),
     ...options,
   })
 }
