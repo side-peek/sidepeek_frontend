9496f2b2b4c7c7b43654cd9b8ab6df6053eae63a
genTe <128919388+wdgWon@users.noreply.github.com>
2024-03-13 19:50:04 +0900

    [Feat ✏️] 회원가입 페이지 UI 구현

---
+++ src/pages/SignUpPage/components/SignUpForm/components/SignUpButton.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@chakra-ui/react"
+
+const SignUpButton = () => {
+  return (
+    <Button
+      type="submit"
+      p="3rem"
+      backgroundColor="blue.100"
+      color="white"
+      fontSize="2.4rem">
+      회원가입
+    </Button>
+  )
+}
+
+export default SignUpButton
