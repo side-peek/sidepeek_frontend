c0e7484c1c529d036a9b454f93c8e4c83d93a11e
이종혁 <whdgur57117@gmail.com>
2024-03-14 21:26:44 +0900

    [Feat ✏️] 유저 검색 API 더미데이터 및 mokcing 추가

---
+++ src/pages/ProjectEditPage/mocks/mockData.ts
@@ -0,0 +1,16 @@
+export const DUMMY_USERSUMMARY = {
+  users: [
+    {
+      id: 1,
+      isSocialLogin: false,
+      nickname: "의진",
+      profileImageUrl: "https://user-images.githubusercontent.com/uijin.png",
+    },
+    {
+      id: 2,
+      isSocialLogin: true,
+      nickname: "의징",
+      profileImageUrl: "https://user-images.githubusercontent.com/uijing.png",
+    },
+  ],
+}
