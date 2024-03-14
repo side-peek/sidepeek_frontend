927a338fcdbfcafa1ca5f460a35043df420a6fde
Whoknow77 <gnsdh86166@gmail.com>
2024-03-15 01:47:35 +0900

    [Refactoring ⚙️] 버튼 전체클릭시 좋아요 요청보내도록 수정

--- src/pages/ProjectDetailPage/components/Summary/SummaryTop/SummaryTop.tsx
+++ src/pages/ProjectDetailPage/components/Summary/SummaryTop/SummaryTop.tsx
@@ -7,9 +7,13 @@ import { Link } from "react-scroll"
 
 import { Flex, useMediaQuery } from "@chakra-ui/react"
 
+// import { useDeleteLikeMutation } from "@pages/ProjectDetailPage/hooks/mutations/useDeleteLikeMutation"
+import { usePostLikeMutation } from "@pages/ProjectDetailPage/hooks/mutations/usePostLikeMutation"
+
+import { ProjectIdProps, withProjectId } from "../../Hoc/withProjectId"
 import SummaryTopIcon from "./SummaryTopIcon"
 
-interface SummaryTopProps {
+interface SummaryTopProps extends ProjectIdProps {
   likeCount: number
   viewCount: number
   commentCount: number
@@ -18,6 +22,7 @@ const SummaryTop = ({
   likeCount,
   viewCount,
   commentCount,
+  projectId,
 }: SummaryTopProps) => {
   /*
     TODO: 1. 좋아요 요청
@@ -33,6 +38,10 @@ const SummaryTop = ({
       console.log(err)
     }
   }
+
+  const { postLikeMutation } = usePostLikeMutation()
+  // const { deleteLikeMutation } = useDeleteLikeMutation()
+
   return (
     <Flex
       gap={isLargerThan1200 ? "1.5rem" : "1rem"}
@@ -50,6 +59,10 @@ const SummaryTop = ({
         icon={<IoMdHeartEmpty />}
         aria-label="likeButton"
         fontSize={isLargerThan1200 ? "2.7rem" : "2rem"}
+        onClick={() => {
+          postLikeMutation.mutate({ projectId: Number(projectId) })
+          // deleteLikeMutation.mutate({ likeId: 12 })
+        }}
       />
       <Link
         to="Comment"
@@ -74,4 +87,4 @@ const SummaryTop = ({
     </Flex>
   )
 }
-export default SummaryTop
+export default withProjectId(SummaryTop)
--- src/pages/ProjectDetailPage/components/Summary/SummaryTop/SummaryTopIcon.tsx
+++ src/pages/ProjectDetailPage/components/Summary/SummaryTop/SummaryTopIcon.tsx
@@ -6,23 +6,12 @@ import {
   useMediaQuery,
 } from "@chakra-ui/react"
 
-import { useDeleteLikeMutation } from "@pages/ProjectDetailPage/hooks/mutations/useDeleteLikeMutation"
-import { usePostLikeMutation } from "@pages/ProjectDetailPage/hooks/mutations/usePostLikeMutation"
-
-import { ProjectIdProps, withProjectId } from "../../Hoc/withProjectId"
-
-interface SummaryTopIconProps extends IconButtonProps, ProjectIdProps {
+interface SummaryTopIconProps extends IconButtonProps {
   count?: number
+  onClick?: () => void
 }
 
-const SummaryTopIcon = ({
-  count,
-  projectId,
-  ...props
-}: SummaryTopIconProps) => {
-  const { postLikeMutation } = usePostLikeMutation()
-  const { deleteLikeMutation } = useDeleteLikeMutation()
-
+const SummaryTopIcon = ({ count, onClick, ...props }: SummaryTopIconProps) => {
   const [isLargerThan1200] = useMediaQuery(["(min-width: 1200px)"])
   const isButton = props["aria-label"].toLowerCase().includes("button")
   const isLike = props["aria-label"].toLowerCase().includes("like")
@@ -31,20 +20,11 @@ const SummaryTopIcon = ({
       gap="0.7rem"
       alignItems="center"
       cursor={isButton ? "pointer" : ""}
-      _hover={isButton ? { opacity: "0.5" } : {}}>
-      {isLike ? (
-        <IconButton
-          onClick={() => {
-            postLikeMutation.mutate({ projectId: Number(projectId) })
-            deleteLikeMutation.mutate({ likeId: 12 })
-          }}
-          {...props}
-        />
-      ) : (
-        <IconButton {...props} />
-      )}
+      _hover={isButton ? { opacity: "0.5" } : {}}
+      onClick={onClick}>
+      {isLike ? <IconButton {...props} /> : <IconButton {...props} />}
       <Text fontSize={isLargerThan1200 ? "xl" : "md"}>{count}</Text>
     </Flex>
   )
 }
-export default withProjectId(SummaryTopIcon)
+export default SummaryTopIcon
