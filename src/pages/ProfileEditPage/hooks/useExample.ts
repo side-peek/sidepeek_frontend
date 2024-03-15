8a066d9eee5f12d74796f3a4b14f20b03f7b8cc9
hellosonic-r <hellommmh@gmail.com>
2024-03-15 09:48:25 +0900

    [Feat ✏️] 기술스택 검색 컴포넌트 사용 훅 추가(수정 예정)

---
+++ src/pages/ProfileEditPage/hooks/useExample.ts
@@ -0,0 +1,50 @@
+import { useFieldArray, useForm } from "react-hook-form"
+
+import { Skill } from "api-models"
+
+import { ProjectFormValues } from "@pages/ProjectEditPage/types/ProjectFormValues"
+
+// TODO: 공통 훅 사용 예정임
+export const useExample = () => {
+  const { control, setValue, getValues, watch } =
+    useForm<Pick<ProjectFormValues, "techStacks">>()
+
+  const { append, fields } = useFieldArray<
+    Pick<ProjectFormValues, "techStacks">
+  >({
+    control,
+    name: "techStacks",
+  })
+
+  const appendNewFields = () => {
+    append({ category: "", stacks: [] })
+  }
+
+  const setCategory = (index: number, value: string) => {
+    setValue(`techStacks.${index}.category`, value)
+  }
+
+  const appendStack = (index: number, element: Skill) => {
+    const stacks = getValues(`techStacks.${index}.stacks`)
+    setValue(`techStacks.${index}.stacks`, [...stacks, element])
+  }
+
+  const removeStack = (index: number, element: Skill) => {
+    const stacks = getValues(`techStacks.${index}.stacks`)
+    const filtered = stacks.filter((stack) => stack.id !== element.id)
+    setValue(`techStacks.${index}.stacks`, [...filtered])
+  }
+
+  const selectedStacks = (index: number) => watch(`techStacks.${index}.stacks`)
+
+  return {
+    fields,
+    watch,
+    control,
+    setCategory,
+    appendNewFields,
+    appendStack,
+    removeStack,
+    selectedStacks,
+  }
+}
