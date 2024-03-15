288eb63a3a9ec4cdffcdb5e5fd0ceef316568c06
genTe <128919388+wdgWon@users.noreply.github.com>
2024-03-15 12:09:53 +0900

    [Feat ✏️] 로그인 페이지 연동 및 msw 테스트 코드 작성

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Center, Divider, Flex } from "@chakra-ui/react"
+import { Box, Center, Divider, Fade, Flex } from "@chakra-ui/react"
 
 import BackButton from "@components/BackButton/BackButton"
 import LogoLink from "@components/LogoLink/LogoLink"
@@ -9,27 +9,29 @@ import SocialLogin from "./components/SocialLogin"
 
 const LoginPage = () => {
   return (
-    <Center height="100vh">
-      <Box
-        aria-label="login container"
-        width="50rem">
-        <Center position="relative">
-          <BackButton />
-          <LogoLink logoHeight="6rem" />
-        </Center>
-        <Flex
-          direction="column"
-          gap="1.6rem"
-          justifyContent="center"
-          alignItems="stretch"
-          padding="2rem">
-          <LoginForm />
-          <Divider />
-          <SocialLogin />
-          <CreateAccount />
-        </Flex>
-      </Box>
-    </Center>
+    <Fade in={true}>
+      <Center height="100vh">
+        <Box
+          aria-label="login container"
+          width="50rem">
+          <Center position="relative">
+            <BackButton />
+            <LogoLink logoHeight="6rem" />
+          </Center>
+          <Flex
+            direction="column"
+            gap="1.6rem"
+            justifyContent="center"
+            alignItems="stretch"
+            padding="2rem">
+            <LoginForm />
+            <Divider />
+            <SocialLogin />
+            <CreateAccount />
+          </Flex>
+        </Box>
+      </Center>
+    </Fade>
   )
 }
 
