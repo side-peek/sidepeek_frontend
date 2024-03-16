18c136db13df66f30d6f1efbbdb28fe92405e8f1
hellosonic-r <hellommmh@gmail.com>
2024-03-16 20:16:58 +0900

    [Refactor ⚙️] 프로필 수정 api 요청 성공 시 본인의 프로필 페이지로 리다이렉팅

--- src/pages/ProfileEditPage/hooks/mutation/usePutUserDetailMutation.ts
+++ src/pages/ProfileEditPage/hooks/mutation/usePutUserDetailMutation.ts
@@ -1,15 +1,20 @@
+import { useNavigate } from "react-router-dom"
+
 import { putUserDetailPayload } from "api-models"
 
 import { useMutation } from "@tanstack/react-query"
 
 import { putUserDetail } from "@apis/user/putUserDetail"
 
-const usePutUserDetailMutation = () => {
+const usePutUserDetailMutation = (userId: number) => {
+  const navigate = useNavigate()
   const putUserDetailMutation = useMutation({
     mutationFn: async (formData: putUserDetailPayload) =>
       await putUserDetail(formData),
     onSuccess(data) {
       console.log("putUserDetail 성공", data)
+      alert("프로필 수정이 완료되었습니다")
+      navigate(`/profile/${userId}`)
     },
   })
   return {
