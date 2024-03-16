a7f70a56a7c39b9d1be811c1b16cddd4a6b9a2f0
Whoknow77 <gnsdh86166@gmail.com>
2024-03-17 00:21:52 +0900

    [Refactoring ⚙️] getprojectdetail 데이터 authinstance => baseinstance

--- src/api/project/getProjectDetail.ts
+++ src/api/project/getProjectDetail.ts
@@ -1,15 +1,21 @@
 import { Project, getProjectDetailPayload } from "api-models"
 import { AxiosRequestConfig } from "axios"
 
+import authToken from "@stores/authToken"
+
 import { ENDPOINTS } from "@constants/endPoints"
 
-import { authInstance } from "../axiosInstance"
+import { baseInstance } from "../axiosInstance"
 
 export const getProjectDetail = async (
   { projectId }: getProjectDetailPayload,
-  config: AxiosRequestConfig = {},
+  config: AxiosRequestConfig = {
+    headers: {
+      Authorization: `Bearer ${authToken.getAccessToken()}`,
+    },
+  },
 ) => {
-  const { data } = await authInstance.get<Project>(
+  const { data } = await baseInstance.get<Project>(
     ENDPOINTS.GET_PROJECT_DETAILS(projectId),
     {
       ...config,
