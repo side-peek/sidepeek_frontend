01aabfca13021d63ab58367c2d1e4199eca5c979
hellosonic-r <hellommmh@gmail.com>
2024-03-17 11:37:38 +0900

    [Refactor ⚙️] 프로필 수정 api 요청이 성공한 후 프로필 페이지로 navigate 시 프로필 정보가 이전 상태로 렌더링 되는 버그 해결

--- src/pages/ProfileEditPage/hooks/mutation/usePutUserDetailMutation.ts
+++ src/pages/ProfileEditPage/hooks/mutation/usePutUserDetailMutation.ts
@@ -2,18 +2,20 @@ import { useNavigate } from "react-router-dom"
 
 import { putUserDetailPayload } from "api-models"
 
-import { useMutation } from "@tanstack/react-query"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 
 import { putUserDetail } from "@apis/user/putUserDetail"
 
 const usePutUserDetailMutation = (userId: number) => {
   const navigate = useNavigate()
+  const queryClient = useQueryClient()
   const putUserDetailMutation = useMutation({
     mutationFn: async (formData: putUserDetailPayload) =>
       await putUserDetail(formData),
     onSuccess(data) {
       console.log("putUserDetail 성공", data)
       alert("프로필 수정이 완료되었습니다")
+      queryClient.invalidateQueries({ queryKey: ["userInfo", userId] })
       navigate(`/profile/${userId}`)
     },
   })
