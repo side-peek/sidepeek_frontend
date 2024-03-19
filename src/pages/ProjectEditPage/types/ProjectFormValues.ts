1ec8bbe035e99ec87457c126f2c861de51ecb97e
이종혁 <whdgur57117@gmail.com>
2024-03-19 10:58:54 +0900

    [Feat ✏️] 에디터 관련 values 추가

--- src/pages/ProjectEditPage/types/ProjectFormValues.ts
+++ src/pages/ProjectEditPage/types/ProjectFormValues.ts
@@ -1,5 +1,11 @@
 import { Skill } from "api-models"
 
+export type FieldMember = {
+  id: number | null
+  nickname: string
+  role: string
+}
+
 export type ProjectFormValues = {
   name: string
   subName: string
@@ -8,9 +14,10 @@ export type ProjectFormValues = {
   thumbnailUrl: string
   githubUrl: string
   deployUrl: string
-  startDate: string //2002-02
-  endDate: string //동일
+  startDate: string
+  endDate: string
   techStacks: { category: string; stacks: Skill[] }[]
-  description: ""
-  troubleShooting: ""
+  description: string
+  troubleShooting: string
+  members: { category: string; members: FieldMember[] }[]
 }
