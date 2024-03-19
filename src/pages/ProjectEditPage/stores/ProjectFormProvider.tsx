ca1094e7e8f617d07814ba51f5225c1cc086327c
이종혁 <whdgur57117@gmail.com>
2024-03-19 10:59:25 +0900

    [Feat ✏️] submitHandler 함수 추가(수정 필요)

--- src/pages/ProjectEditPage/stores/ProjectFormProvider.tsx
+++ src/pages/ProjectEditPage/stores/ProjectFormProvider.tsx
@@ -1,14 +1,47 @@
 import { PropsWithChildren } from "react"
 import { FormProvider } from "react-hook-form"
 
+import { useUserInfoData } from "@services/caches/useUserInfoData"
+
+import { usePostProjectMutation } from "../hooks/usePostProjectMutation"
 import { useProjectForm } from "../hooks/useProjectForm"
+import { ProjectFormValues } from "../types/ProjectFormValues"
 
 const ProjectFormProvider = ({ children }: PropsWithChildren) => {
   const methods = useProjectForm()
+  const { mutate } = usePostProjectMutation()
+  //TODO : 인증 관련 로직 수정 필요
+  const userInfo = useUserInfoData()
+  if (!userInfo) {
+    throw new Error()
+  }
+
+  const handleSubmitEvent = (data: ProjectFormValues) => {
+    const members = data.members
+      .map(({ category, members }) => {
+        return members.map(({ id, nickname }) => {
+          if (!members.length) {
+            methods.setError("members", { message: " 하나 이상 넣으세요" })
+          }
+          return {
+            id,
+            nickname,
+            role: category,
+          }
+        })
+      })
+      .flat()
+    const techStacks = data.techStacks
+      .map(({ category, stacks }) => {
+        return stacks.map(({ id }) => ({ skillId: id, category }))
+      })
+      .flat()
+    mutate({ ...data, members, techStacks, ownerId: userInfo.id as number })
+  }
 
   return (
     <FormProvider {...methods}>
-      <form onSubmit={methods.handleSubmit((data) => console.log(data))}>
+      <form onSubmit={methods.handleSubmit((data) => handleSubmitEvent(data))}>
         {children}
       </form>
     </FormProvider>
