cdc38023134cedb2b34095181f6635a3a8c76361
hellosonic-r <hellommmh@gmail.com>
2024-03-21 09:07:59 +0900

    [Refactor ⚙️] 닉네임 변경 validation 옵션 동건님과 통일

--- src/pages/ProfileEditPage/constants/validation.ts
+++ src/pages/ProfileEditPage/constants/validation.ts
@@ -10,9 +10,17 @@ export const RegEx = {
 
 export const NICKNAME_VALIDATION_OPTION = {
   required: "새로운 닉네임을 입력해주세요",
+  pattern: {
+    value: /^[a-zA-Z0-9가-힣]+$/,
+    message: "닉네임은 특수문자를 사용할 수 없습니다",
+  },
+  maxLength: {
+    value: 12,
+    message: "닉네임은 최대 12글자까지 가능합니다",
+  },
   minLength: {
     value: 2,
-    message: "새로운 닉네임은 2자 이상이어야 합니다",
+    message: "닉네임은 최소 2글자 이상 입력하여야 합니다",
   },
 }
 
@@ -23,10 +31,10 @@ export const NICKNAME_NOSPACE_ERROR = {
 
 export const CURRENT_PASSWORD_VALIDATION_OPTION = {
   required: "현재 비밀번호를 입력해주세요",
-  minLength: {
-    value: 8,
-    message: "현재 비밀번호는 8자 이상입니다",
-  },
+  // minLength: {
+  //   value: 8,
+  //   message: "현재 비밀번호는 8자 이상입니다",
+  // },
 }
 
 export const NEW_PASSWORD_VALIDATION_OPTION = {
